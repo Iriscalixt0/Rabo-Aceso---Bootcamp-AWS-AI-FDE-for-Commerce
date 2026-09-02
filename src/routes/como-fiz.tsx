05714513f4676a7fb7180b88bc86092b6593de74
Iriscalixt0 <thamiriscalixtob@gmail.com>
2026-09-01 22:27:23 -0300

    url video

--- src/routes/como-fiz.tsx
+++ src/routes/como-fiz.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, Link } from "@tanstack/react-router";
 
 import { Page } from "@/components/StoreChrome";
 
-const VIDEO_EMBED_URL = "";
+const VIDEO_EMBED_URL = "https://youtu.be/QnLHFwRTioU";
 
 export const Route = createFileRoute("/como-fiz")({
   head: () => ({
